3c87b160259b996499f2402dd0735dab493518a2
nonSerp <fredfarwenel2010@gmail.com>
2026-02-03 20:46:00 +0000

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gbengbartown.com',
   output: 'static',
+  adapter: vercel(),
 });
