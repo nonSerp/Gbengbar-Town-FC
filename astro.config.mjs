bf5cc726080ded6f0ad5ab5f70ee9da274da20f6
nonSerp <fredfarwenel2010@gmail.com>
2026-02-12 12:50:59 +0000

    SocialIcons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap'
 import vercel from '@astrojs/vercel';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gbengbartown.com',
   integrations: [
-    sitemap()
+    sitemap(),
+    icon()
   ],
   output: 'static',
   adapter: vercel(),
